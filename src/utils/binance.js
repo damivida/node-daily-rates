824121990fafd434c35b371c7f49d0c7dc61e7dd
Damir Vidaković <vida.damir@gmail.com>
2020-01-19 11:05:26 +0100

    test code from binance module deleted

--- src/utils/binance.js
+++ src/utils/binance.js
@@ -29,30 +29,5 @@ let url = `https://api.binance.com/api/v1/klines?symbol=${asset}BTC&interval=1d&
 
 module.exports = binanceApi;
 
-// BINANCE API AXIOS(PROMISE BASED)
-/* const binanceApi = (time, asset) => {
 
-   let url = `https://api.binance.com/api/v1/klines?symbol=${asset}BTC&interval=1d&startTime=${time}`;
-    //let url = 'https://api.binance.com/api/v1/klines?symbol=XMRBTC&interval=1d&startTime=1578873600000';
-
-    axios.get(url).then(response => {
-
-     const binanceRates = {
-           open: response.data[0][1],
-           high: response.data[0][2],
-           low: response.data[0][3],
-           close: response.data[0][4],
-           volume: response.data[0][5],
-       }  
-
-       //console.log(binaceRates);
-       return binanceRates.open;
-       
-        
-    }).catch(error => console.error);
-
-    
-}
- */
-//binanceApi();
 
