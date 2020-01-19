724313bb027edc11cc5a53a3e8f29a8908aa78bb
Damir Vidaković <vida.damir@gmail.com>
2020-01-19 11:07:31 +0100

    new module for hitbtc API created

---
+++ src/utils/hitbtc.js
@@ -0,0 +1,36 @@
+const request =  require('request');
+
+//CALLBACK FUNCTION (HITBTC API)
+
+const hitBtcApi = (time, asset, callback) => {
+
+
+    let url = `https://api.hitbtc.com/api/2/public/candles/${asset}BTC?from=${time}&period=D1`;
+
+    //let url = `https://api.hitbtc.com/api/2/public/candles/ETNBTC?from=1579046400000&period=D1`;
+
+
+    request({url, json:true}, (error, {body}) => {
+        if(error) {
+            callback('Unable to connect to location services!', undefined);
+        }else if(body.error || body.length === 0) {
+            callback('Unable to find rates', undefined);
+        }else {
+           
+            callback(undefined, {
+                open: parseFloat(body[0].open),
+                high: parseFloat(body[0].max),
+                low: parseFloat(body[0].min),
+                close: parseFloat(body[0].close),
+                volume: parseFloat(body[0].volumeQuote)
+
+            })
+        }
+    });
+
+
+}
+
+
+
+module.exports = hitBtcApi;
